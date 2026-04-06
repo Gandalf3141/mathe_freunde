c5b08388c45c0a29a41d1e5c57bb052dbb9a4a7e
Gandalf3141 <paul.strasser98@gmail.com>
2026-04-06 16:53:22 +0200

    wordlist added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from src!")
-    with open(r"C:\Users\Paul\Documents\Python Projects\mathe_freunde\wordlist.txt") as f:
+    with open(r"wordlist.txt") as f:
         print(f"the official wordlist countains {len([x for x in f])} - 5 letter words!")
 
 
